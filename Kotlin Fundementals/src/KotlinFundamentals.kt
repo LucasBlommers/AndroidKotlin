35435154087d27a9465367c56a7ddf388b7a9667
lucas <lucas-blommers@live.nl>
2021-09-22 19:16:34 +0200

    Made first exercise: Variables and datatypes

---
+++ Kotlin Fundementals/src/KotlinFundamentals.kt
@@ -0,0 +1,8 @@
+
+fun main(){
+    //Exercise Variables and Datatypes
+    var name:String = "Android Masterclass"
+    var float:Float = 13.37F
+    var double:Double = Math.PI
+    var int:Int = 25
+}
