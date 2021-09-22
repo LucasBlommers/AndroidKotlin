084a6d113974fdf65a72a2a756f76ab365140ccf
lucas <lucas-blommers@live.nl>
2021-09-22 19:25:07 +0200

    Made first exercise: Variables and datatypes

--- Kotlin Fundementals/src/KotlinFundamentals.kt
+++ Kotlin Fundementals/src/KotlinFundamentals.kt
@@ -5,4 +5,8 @@ fun main(){
     var float:Float = 13.37F
     var double:Double = Math.PI
     var int:Int = 25
+    var year:Int = 2020
+    var long:Long = 18881234567
+    var boolean:Boolean = true
+    var char:Char = 'a'
 }
