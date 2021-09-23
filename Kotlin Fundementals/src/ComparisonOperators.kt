faff1be0de60ae611b75b0cb2abb101e6dbd2111
lucas <lucas-blommers@live.nl>
2021-09-23 10:19:51 +0200

    Added ComparisonOperators.kt

---
+++ Kotlin Fundementals/src/ComparisonOperators.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    //Comparison Operators (==, !=, >, <, <=, >=)
+    val isEqual = 5==3
+    println("isEqueal is $isEqual")
+
+    val isNotEqual = 5!=5
+    println("isNotEqual is $isNotEqual")
+
+    println("is5Greater3 ${5>3}")
+
+}
