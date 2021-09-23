0c1c6a724acc1d2f806481481656b0708a8504b4
lucas <lucas-blommers@live.nl>
2021-09-23 16:26:30 +0200

    Updated ScopeAndShadowing.kt to make shadowing more clear

--- OOPKotlin/src/ScopeAndShadowing.kt
+++ OOPKotlin/src/ScopeAndShadowing.kt
@@ -5,6 +5,9 @@ fun main(){
 //This a is a parameter
 fun myFunction(a:Int){
     //a is a variable
+    var a = 10
     var b = 4
+
+    println("var a: $a is now ghosting parameter a which is no longer accesible.")
     println("b is $b")
 }
