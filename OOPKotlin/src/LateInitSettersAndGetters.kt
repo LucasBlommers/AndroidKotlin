44966d84fdd7f0675839d17f13f24a87f47dbdee
lucas <lucas-blommers@live.nl>
2021-09-23 17:14:20 +0200

    Added LateInitSettersAndGetters.kt

---
+++ OOPKotlin/src/LateInitSettersAndGetters.kt
@@ -0,0 +1,28 @@
+
+fun main(){
+    var myCar = Car()
+    myCar.maxSpeed = 200
+    println("Max speed is: ${myCar.maxSpeed}")
+    println("Model is ${myCar.myModel}")
+}
+
+class Car(){
+    lateinit var owner:String
+    val myBrand:String = "BMW"
+    get(){
+        return field.lowercase()
+    }
+
+    var maxSpeed: Int = 250
+    set(value) {
+        field = if(value > 0) value else throw IllegalArgumentException("Maximum speed cannot be less than zero")
+    }
+
+    var myModel: String = "M5"
+    private set
+
+    init {
+        this.owner = "Frank"
+        this.myModel = "M3"
+    }
+}
