d8b3ab65272ffccf433c0543f87824ce5640dbc9
lucas <lucas-blommers@live.nl>
2021-09-23 09:58:35 +0200

    Added BoolCharString.kt which holds the code on the part which was about Booleans characters and strings

---
+++ Kotlin Fundementals/src/BoolCharString.kt
@@ -0,0 +1,15 @@
+
+fun main(){
+    var isSunny: Boolean = true
+    isSunny = false
+    //Characters
+    val letterChar = 'A'
+    val digitChar = '1'
+
+    //Strings
+    val myString = "Hello world"
+    var firstCharInStr = myString[0]
+    var lastCharInStr = myString[myString.length - 1]
+
+    println("$lastCharInStr")
+}
--- Kotlin Fundementals/src/KotlinFundamentals.kt
+++ Kotlin Fundementals/src/KotlinFundamentals.kt
@@ -1,7 +1,29 @@
 
 fun main(){
+
+    //Val and var
+    //Type inference
+    val name = "Frank"
+    var otherName = "Henk"
+    println("Hello $name and $otherName")
+    otherName = "Jan"
+
+    println("Hello $otherName")
+
+    //Integer TYPES: Byte (8 bit), Short (16 bit),
+    //Int (32 bit), Long (64 bit)
+
+    val myByte: Byte = 13
+    val myShort: Short = 125
+    val myInt: Int = 123123123
+    val myLong: Long = 12_039_145_659_567
+
+    //Float types: Float (32 bit) Double (64 bit)
+    val myFloat: Float = 13.37F
+    val myDouble: Double = Math.PI
+
     //Exercise Variables and Datatypes
-    var name:String = "Android Masterclass"
+    var projectName:String = "Android Masterclass"
     var float:Float = 13.37F
     var double:Double = Math.PI
     var int:Int = 25
@@ -9,4 +31,11 @@ fun main(){
     var long:Long = 18881234567
     var boolean:Boolean = true
     var char:Char = 'a'
+
+    //Exercise Loops
+    for (i in 0 .. 10000){
+        if (i > 9000){
+            println("IT'S OVER 9000!!!")
+        }
+    }
 }
