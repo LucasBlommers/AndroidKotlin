6fe58f9ee524b52c102682002181b63a6a3a5ece
lucas <lucas-blommers@live.nl>
2021-09-23 10:55:17 +0200

    Added AssignmentOperators.kt

---
+++ Kotlin Fundementals/src/AssignmentOperators.kt
@@ -0,0 +1,17 @@
+fun main(){
+    //Assignment Operators (+=, -=, *=, /=, %=)
+    var myNum = 5
+    myNum += 3
+
+    println("myNum is $myNum")
+
+    myNum *= 4
+    println("myNum is $myNum")
+
+    myNum--
+    println("myNum is $myNum")
+
+    println("myNum is ${++myNum}")
+
+
+}
