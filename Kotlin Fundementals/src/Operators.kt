6f53976e1578c9385b11d6b3de0a11917d1a0cf0
lucas <lucas-blommers@live.nl>
2021-09-23 10:09:09 +0200

    Added Operators.kt

---
+++ Kotlin Fundementals/src/Operators.kt
@@ -0,0 +1,15 @@
+fun main(){
+    //Arithmetic operators (+, -, *, /, %)
+    var result = 5 + 3
+
+    result /= 2
+
+    result = 15%3
+
+    val a = 5.0
+    val b = 3.0
+    var resultDouble: Double
+    resultDouble = a / b
+
+    println("$resultDouble")
+}
